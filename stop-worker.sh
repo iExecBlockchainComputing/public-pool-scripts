d7b49a5538cd194f39c96ec2fcdbecdf14d174f7
Vladimir Ostapenco <vladost.pro@gmail.com>
2019-05-14 15:59:51 +0200

    Warning

--- stop-worker.sh
+++ stop-worker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-echo "[WARNING] Don't forget to store your wallet from the keystore!"
+echo "------------------------------------------------------------------------"
+echo "[WARNING] Don't forget to store your wallet from the keystore directory!"
+echo "------------------------------------------------------------------------"
 /bin/bash /home/iexec/iexec-worker/public/launch-worker.sh --remove
